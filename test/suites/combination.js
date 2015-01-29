89050753d4d9321fdb046e07a9e3efada54e7193
Yomguithereal <guillaumeplique@gmail.com>
2015-01-29 21:21:23 +0100

    Adding combination unit tests

--- test/suites/combination.js
+++ test/suites/combination.js
@@ -169,4 +169,72 @@ describe('Combination', function() {
     assert.strictEqual(count, 1);
     combination.release();
   });
+
+  it('should be possible to use some polymorphism.', function() {
+    var combination = new Combination('or', [cursor, othercursor]),
+        count = 0;
+
+    combination.on('update', function() {
+      count++;
+    });
+
+    // 1
+    cursor.push(4);
+    baobab.commit();
+
+    // 2
+    othercursor.push(7);
+    baobab.commit();
+
+    // 3
+    cursor.edit([1]);
+    othercursor.edit([4]);
+    baobab.commit();
+
+    assert.strictEqual(count, 3);
+    combination.release();
+
+    var combination = new Combination('or', cursor, othercursor),
+        count = 0;
+
+    combination.on('update', function() {
+      count++;
+    });
+
+    // 1
+    cursor.push(4);
+    baobab.commit();
+
+    // 2
+    othercursor.push(7);
+    baobab.commit();
+
+    // 3
+    cursor.edit([1]);
+    othercursor.edit([4]);
+    baobab.commit();
+
+    assert.strictEqual(count, 3);
+    combination.release();
+  });
+
+  it('a single cursor combination should work as expected.', function() {
+    var combination = new Combination('or', cursor),
+        count = 0;
+
+    combination.on('update', function() {
+      count++;
+    });
+
+    // 1
+    cursor.push(4);
+    baobab.commit();
+
+    // 2
+    cursor.push(5);
+    baobab.commit();
+
+    assert.strictEqual(count, 2);
+    combination.release();
+  });
 });
