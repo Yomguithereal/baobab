e921c226603b1c92016f0a27d8fb45fc7ce6c2f2
Yomguithereal <guillaumeplique@gmail.com>
2015-04-09 23:33:39 +0200

    Style normalization in type.js

--- src/type.js
+++ src/type.js
@@ -21,19 +21,19 @@ function anyOf(value, allowed) {
 /**
  * Simple types
  */
-type.Array = function (value) {
+type.Array = function(value) {
   return Array.isArray(value);
 };
 
-type.Object = function (value) {
+type.Object = function(value) {
   return !Array.isArray(value) && typeof value === 'object' && value !== null;
 };
 
-type.String = function (value) {
+type.String = function(value) {
   return typeof value === 'string';
 };
 
-type.Number = function (value) {
+type.Number = function(value) {
   return typeof value === 'number';
 };
 
@@ -41,15 +41,15 @@ type.PositiveInteger = function(value) {
   return typeof value === 'number' && value > 0 && !(value % 1);
 };
 
-type.Function = function (value) {
+type.Function = function(value) {
   return typeof value === 'function';
 };
 
-type.Primitive = function (value) {
+type.Primitive = function(value) {
   return typeof value === 'string' || typeof value === 'number' || typeof value === 'boolean';
 };
 
-type.Date = function (value) {
+type.Date = function(value) {
   return value instanceof Date;
 };
 
@@ -60,7 +60,7 @@ type.NonScalar = function(value) {
   return type.Object(value) || type.Array(value);
 };
 
-type.Path = function (value) {
+type.Path = function(value) {
   var allowed = ['String', 'Number', 'Function', 'Object'];
 
   if (type.Array(value)) {
@@ -73,15 +73,15 @@ type.Path = function (value) {
   }
 };
 
-type.MixinCursor = function (value) {
+type.MixinCursor = function(value) {
   return anyOf(value, ['String', 'Number', 'Array', 'Function', 'Cursor']);
 };
 
-type.MixinCursors = function (value) {
+type.MixinCursors = function(value) {
   return anyOf(value, ['Object', 'Array', 'Function']);
 };
 
-type.ComplexPath = function (value) {
+type.ComplexPath = function(value) {
   return value.some(function(step) {
     return anyOf(step, ['Object', 'Function']);
   });
