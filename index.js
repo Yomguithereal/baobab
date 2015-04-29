79aae60ec8a1c666912094dec206c5e01f2115cc
charlieschwabacher <charlieschwabacher@gmail.com>
2015-04-29 13:55:40 -0500

    expose Cursor class for type checking

--- index.js
+++ index.js
@@ -5,12 +5,16 @@
  * Exposes the main library classes.
  */
 var Baobab = require('./src/baobab.js'),
+    Cursor = require('./src/cursor.js'),
     helpers = require('./src/helpers.js');
 
 // Non-writable version
 Object.defineProperty(Baobab, 'version', {
   value: '1.0.2'
 });
+
+// Exposing Cursor class
+Baobab.Cursor = Cursor;
 
 // Exposing helpers
 Baobab.getIn = helpers.getIn;
