e6afc4aa47c6c37b6012d69094052eda17deaa3b
Luke Harold Miles <luke@cs.uky.edu>
2021-10-31 16:54:45 -0400

    add types.ts stub

---
+++ test/suites/types.ts
@@ -0,0 +1,8 @@
+import Baobab, {Cursor} from '../../src/baobab';
+import {strict as assert} from 'assert';
+
+describe('Types', function() {
+    it('should have all the right types but this test doesn\'t actually test that', function() {
+        assert.strictEqual(2 + 2, 4);
+    });
+});
